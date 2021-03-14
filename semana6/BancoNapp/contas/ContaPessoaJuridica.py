0c16dc098f62c81403a94cda3cd5227bd28c68f5
Nathalia Tesch <natalia.tesch@nappsolutions.com>
2021-03-14 14:07:07 -0300

    semana6

--- semana6/BancoNapp/contas/ContaPessoaJuridica.py
+++ semana6/BancoNapp/contas/ContaPessoaJuridica.py
@@ -2,4 +2,15 @@ from BancoNapp.contas.Conta import Conta
 
 
 class ContaPessoaJuridica(Conta):
-    pass
+    """
+    Args:
+        Conta ([type]): [description]
+    """
+    def __init__(self,  **kwargs):
+        """
+        Construtor da classe PessoaFísica.
+        Extrai do dicionário kwargs a profissao do correntista.
+        """
+        super(ContaPessoaJuridica, self).__init__(**kwargs)
+        self.empresa = kwargs.get('empresa', '')
+        self.limite = kwargs.get('limite', 1500)
--- semana6/BancoNapp/contas/ContaPoupanca.py
+++ semana6/BancoNapp/contas/ContaPoupanca.py
@@ -2,4 +2,20 @@ from BancoNapp.contas.Conta import Conta
 
 
 class ContaPoupanca(Conta):
-   pass
+    """
+    Args:
+        Conta ([type]): [description]
+    """
+    def __init__(self,  **kwargs):
+        """
+        Construtor da classe ContaPoupanca.
+        Extrai do dicionário kwargs a profissao do correntista.
+        """
+        super(ContaPoupanca, self).__init__(**kwargs)
+        self.profissao = kwargs.get('profissao', '')
+        self.limite = kwargs.get('limite', 0)
+
+    def rendimento_aniversario(self, valor):
+        if valor < 0 or valor > 1:
+            raise ValueError('Os juros precisam ser entre 0 (0%) e 1 (100%).')
+        self.saldo += self.saldo*valor
